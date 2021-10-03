04a59033a3ed609346e1ae29018e3271a669e73f
TJain02 <86570360+TJain02@users.noreply.github.com>
2021-10-03 19:32:29 +0530

    procesing part done

--- lucky.js
+++ lucky.js
@@ -1,7 +1,31 @@
 const DateOfBirth = document.querySelector("#d-o-b");
 const LuckyNumber = document.querySelector("#lucky-num");
 const checkNumButton = document.querySelector("#check-num");
+const resultBox = document.querySelector("#result-box")
 
- checkNumButton.addEventListener("click", function clickEventHandler(){
-    console.log(DateOfBirth.value , LuckyNumber.value);
-});
+function compareValues(sum, LuckyNumber){
+    if(sum % LuckyNumber.value ===0){
+        resultBox.innerText="Your birthday is lucky 🎉";
+    } else {
+        resultBox.innerText="Your birhday is not lucky 🤤"
+    }
+}
+function checkBirthDateIsLucky(){
+    const bdate = DateOfBirth.value;
+    const sum = calculateSum(bdate);
+    if(sum&&bdate)
+    compareValues(sum,LuckyNumber.value);
+    else
+    resultBox.innerText="Please enter both the fields!";
+}
+
+function calculateSum(bdate){
+    bdate = bdate.replaceAll("-","");
+    let sum=0;
+    for( let i=0; i<bdate.length;i++){
+        sum = sum + Number( bdate.charAt(i));
+    }
+    return sum;
+}
+
+ checkNumButton.addEventListener("click", checkBirthDateIsLucky)
